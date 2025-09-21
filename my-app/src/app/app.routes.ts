794596710d9fae1d8097d6c798a8bb8b46891e0a
dennis42702 <dennis42702@gmail.com>
2025-09-21 19:53:17 -0400

    added landing page, signup, login; also added routing

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Landing } from './landing/landing'
+import { Signup } from './signup/signup' 
+import { Login } from './login/login' 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Landing,
+        pathMatch: 'full',
+    }, 
+    {
+        path: 'login',
+        component: Login,
+
+    },
+    {
+        path:'signup',
+        component: Signup,
+    }
+
+
+
+
+
+];
