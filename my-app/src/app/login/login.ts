aeaf0893da2fe08489eef7d3ce6b07904b3378b7
Ji Bing Ni <jibingni17@gmail.com>
2025-09-21 23:08:08 -0400

    added basic login/signup ui

--- my-app/src/app/login/login.ts
+++ my-app/src/app/login/login.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterModule],
   standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  email = '';
+  password = '';
 
+  onLogin() {
+    alert(`Logging in...\nEmail: ${this.email}`);
+  }
 }
--- my-app/src/app/signup/signup.ts
+++ my-app/src/app/signup/signup.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule, RouterModule],
   standalone: true,
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
 export class Signup {
+  
+  email: string = '';
+  password: string = '';
+
+  onSignup() {
+    alert(`Signing up with\nEmail: ${this.email}\nPassword: ${this.password}`);
+  }
 
 }
